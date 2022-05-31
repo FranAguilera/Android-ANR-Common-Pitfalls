8e2539390034f214cdd256fae1834a2c8d62783e
Francisco Aguilera <fran.aguilera@uber.com>
2022-05-30 21:49:45 -0700

    Set replace policy for workermanager

--- app/src/main/java/com/franjam/anr/common_anr_pitfalls/work_manager/RxWorkerScheduler.kt
+++ app/src/main/java/com/franjam/anr/common_anr_pitfalls/work_manager/RxWorkerScheduler.kt
@@ -1,17 +1,18 @@
 package com.franjam.anr.common_anr_pitfalls.work_manager
 
 import android.content.Context
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import androidx.work.WorkRequest
 
 class RxWorkerScheduler() {
     fun scheduleWork(context: Context) {
-        val rxDownloadWorker: WorkRequest =
+        val rxDownloadWorker: OneTimeWorkRequest =
             OneTimeWorkRequestBuilder<RxDownloadWorker>()
                 .build()
         WorkManager
             .getInstance(context)
-            .enqueue(rxDownloadWorker)
+            .enqueueUniqueWork("RxWorkerScheduler", ExistingWorkPolicy.REPLACE, rxDownloadWorker)
     }
 }
